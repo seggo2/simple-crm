04447a49e953615e092905c4f523b6d1b0a10a1b
Sefa Gür <sefa_guer@hotmail.com>
2024-03-12 21:32:24 +0100

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet,RouterModule } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatSidenavModule,MatIconModule,RouterModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'simplecrm';
+  screenWidth!: number;
+  innerWidth = false
 
+  constructor() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.screenWidth = window.innerWidth;
+    this.innerWidth=false;
+    if (this.screenWidth < 700) {
+      this.innerWidth=true;
+    }
+  }
 }
